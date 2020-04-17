0e8369daa26bb01802cd7cbe21b92bebeef0fe39
Abhinandan Mittal <abhimittal342@gmail.com>
2020-04-17 13:46:16 +0530

    covid form added

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import './App.css';
+import CovidForm from './covidForm';
+class App extends Component {
+  
+ 
+ 
+render(){
+
+  
+ return (
+  <div style={{padding:'50px'}}>
+   <CovidForm />
+   </div>
+  
+  
+  );
+
+}
+
+}
+
+export default App;
